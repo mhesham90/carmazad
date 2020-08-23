4002162b826a0932debd385272cc89b54fb0c327
mhesham90 <mohamed.hisham@tajawal.com>
2020-08-23 20:43:10 +0400

    Updates

---
+++ precache-manifest.d5e40a740a582a84ad37a2f2810af19d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb7e51aa924034b573a850cab39b613",
+    "url": "/carmazad/index.html"
+  },
+  {
+    "revision": "76e4230bbec6464fea9d",
+    "url": "/carmazad/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "46e4b99def9801f15fc7",
+    "url": "/carmazad/static/js/2.95436bd8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/carmazad/static/js/2.95436bd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e4230bbec6464fea9d",
+    "url": "/carmazad/static/js/main.26da975f.chunk.js"
+  },
+  {
+    "revision": "f7deb8bdfed1971f7bf8",
+    "url": "/carmazad/static/js/runtime-main.ccedc530.js"
+  },
+  {
+    "revision": "181f277a3b153d81f9bf94ff0bae91d3",
+    "url": "/carmazad/static/media/logo2.181f277a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/carmazad/precache-manifest.695934d7dc5a0a8d982076fa57af0ce1.js"
+  "/carmazad/precache-manifest.d5e40a740a582a84ad37a2f2810af19d.js"
 );
 
 self.addEventListener('message', (event) => {
