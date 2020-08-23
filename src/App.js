d6c86dfa8e270df68acdb5debb8ee8d40857fb9e
mhesham90 <mohamed.hisham@tajawal.com>
2020-08-23 20:52:25 +0400

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
 import TopBar from "./views/TopBar";
 import { getTheme } from './styles/theme';
@@ -16,10 +16,12 @@ function App() {
     <ThemeProvider theme={getTheme()}>
       <Router basename={process.env.PUBLIC_URL}>
         <TopBar />
+        <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/listing" component={Listing} />
           <Route path="/details" component={Details} />
           <Route path="/sellcar" component={SellCar} />
+        </Switch>
       </Router>
     </ThemeProvider>
   );
