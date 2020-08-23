e9d109f1cf2034935bc7a4ff6a1d7bbde183d34e
mhesham90 <mohamed.hisham@tajawal.com>
2020-08-23 15:41:00 +0400

    Updates

---
+++ precache-manifest.6c735aeb0bdbe533d792538aa95fa623.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef0945385d3e4cff4001dce41ab2940",
+    "url": "/carmazad/index.html"
+  },
+  {
+    "revision": "afd252eaf386b35f247f",
+    "url": "/carmazad/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9cdc27e9d3f1135b8fb",
+    "url": "/carmazad/static/js/2.baef6389.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carmazad/static/js/2.baef6389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd252eaf386b35f247f",
+    "url": "/carmazad/static/js/main.0f10a64f.chunk.js"
+  },
+  {
+    "revision": "f7deb8bdfed1971f7bf8",
+    "url": "/carmazad/static/js/runtime-main.ccedc530.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/carmazad/static/media/logo.5d5d9eef.svg"
+  }
+]);
